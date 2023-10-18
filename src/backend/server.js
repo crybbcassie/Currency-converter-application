def6b981a6dd46e37291eb6239e7b3600b7e515e
Cassie <crybbcassie@gmail.com>
2023-10-18 18:41:04 +0300

    fix backend api request

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const cron = require("cron");
 const { db } = require("./firebase");
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.REACT_APP_SERVER_PORT || 3005;
 
 const app = express();
 
@@ -13,13 +13,24 @@ app.use(express.json());
 
 const fetchConversionRates = async () => {
   try {
-    const response = await axios.get(
-      `https://v6.exchangerate-api.com/v6/${process.env.REACT_APP_API_KEY}/latest/USD`
-    );
-    
-    const rates = response?.data?.conversion_rates;
 
-    if (rates && Object.keys(rates).length > 0) {
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    headers: {
+      apiKey: "IcwmX4syP11HBuRrlfU4PPBH4lfytWeT",
+    },
+  };
+
+  const response = await axios.get(
+    "https://api.apilayer.com/currency_data/live?source=USD&currencies=",
+    requestOptions
+  );
+
+    const rates = response?.data?.quotes;
+    console.log(rates)
+
+    if (rates) {
       const conversionRatesRef = db
         .collection("currency")
         .doc("conversion_rates");
@@ -34,9 +45,15 @@ const fetchConversionRates = async () => {
   }
 };
 
-const cronJob = new cron.CronJob("0 */4 * * *", fetchConversionRates);
+// const cronJob = new cron.CronJob("0 */4 * * *", fetchConversionRates);
 
-cronJob.start();
+// cronJob.start();
+
+app.get(`/fire`, async (req, res) => {
+  const data = await fetchConversionRates();
+  console.log(data);
+  res.send(data);
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
